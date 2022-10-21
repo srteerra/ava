52264ffe515499d7804b031f911cb900bfcff7e5
Angel Lopez <srterradev@gmail.com>
2022-10-20 23:02:23 -0600

    adding fleek deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ./public
+cd ./dist
 FILES=$(find * -type f | awk -v q="'" '{print " -F " q "file=@\"" $0 "\";filename=\"" $0 "\"" q}')
 curl "https://ipfs.infura.io:5001/api/v0/add?pin=true&recursive=true&wrap-with-directory=true&cid-version=1" -vv -X POST $FILES
 cd ..
